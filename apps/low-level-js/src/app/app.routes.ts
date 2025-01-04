7967700ee1b0693c8247efdb6b408fc1b1bca11f
warnerb47 <taphagaye2001.mg@gmail.com>
2025-01-04 20:02:53 +0000

    feat: add readDir

--- apps/low-level-js/src/app/app.routes.ts
+++ apps/low-level-js/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-    { path: '', redirectTo: 'explorer' },
+    { path: '', pathMatch: 'full', redirectTo: 'explorer' },
     { path: 'explorer', loadComponent: () => import('./modules/explorer/explorer.component').then(m => m.ExplorerComponent) }
 ];
--- apps/low-level-js/src/app/modules/explorer/explorer.component.ts
+++ apps/low-level-js/src/app/modules/explorer/explorer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { readDir, BaseDirectory } from '@tauri-apps/api/fs';
+import { readDir, BaseDirectory } from '@tauri-apps/api/fs';
 
 @Component({
   selector: 'app-explorer',
@@ -13,11 +13,10 @@ export class ExplorerComponent implements OnInit {
   }
 
   async readDir() {
-    
-    // const entries = await readDir('users', { dir: BaseDirectory.AppData, recursive: true });
-    // for (const entry of entries) {
-    //   console.log(`Entry: ${entry.path}`);
-    // }
+    const entries = await readDir('Desktop', { dir: BaseDirectory.Home, recursive: false });
+    for (const entry of entries) {
+      console.log(`Entry: ${entry.path}`);
+    }
 
   }
 }
